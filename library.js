d020ad0403e5890ee881b6a3636c775e6932e55c
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-01-20 13:54:02 +0800

    almost done. just missing some elements (80%)

--- library.js
+++ library.js
@@ -1,4 +1,11 @@
 let myLibrary = [];
+let book = "";
+
+let addBook = document.querySelector('#add-book');
+let bookTitle = "";
+let bookAuthor = "";
+let numPages = "";
+let haveRead = document.getElementById('read-status');
 
 function Book(title,author,pages){
     this.title = title;
@@ -11,5 +18,19 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-const book1 = new Book("The Pirates of the Carribean", "John Pogi", "300 pages");
-addBookToLibrary(book1);
+addBook.addEventListener("click", (e) =>
+{
+    e.preventDefault();
+    bookTitle = document.getElementById('book-title').value;
+    bookAuthor = document.getElementById('author').value;
+    numPages = document.getElementById('num-pages').value;
+    book = new Book(bookTitle,bookAuthor,numPages);
+    addBookToLibrary(book);
+
+    console.log(book);
+});
+
+
+//sample library
+book = new Book("The Pirates of the Carribean", "John Pogi", "300 pages");
+addBookToLibrary(book);
