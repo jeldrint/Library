e2b9298cc3dc7eb523b40c8c8c96e78986df4b77
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-01-23 04:51:19 +0800

    revised, so progress is reset (60%)

--- library.js
+++ library.js
@@ -1,36 +1,88 @@
-let myLibrary = [];
+let myLibrary = [ {
+    title: "The Chronicles Of Narnia",
+    author: "C.S. Lewis",
+    pages: "848 pages",
+    haveRead: "read"
+},
+{
+    title: "Don Quixote",
+    author: "Miguel De Cervantes",
+    pages: "566 pages",
+    haveRead: "not yet read"
+},
+{
+    title: "A Tale of Two Cities",
+    author: "Charles Dickens",
+    pages: "859 pages",
+    haveRead: "not yet read"
+},
+{
+    title: "The Little Prince (Le Petit Prince)",
+    author: "Antoine de Saint-Exupery",
+    pages: "643 pages",
+    haveRead: "not yet read"
+},
+{
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: "463 pages",
+    haveRead: "not yet read"
+},
+];
+
 let book = "";
+let len = 0;
 
 let addBook = document.querySelector('#add-book');
-let bookTitle = "";
-let bookAuthor = "";
-let numPages = "";
-let haveRead = document.getElementById('read-status');
+let removeBook = document.querySelector('#remove-book');
 
-function Book(title,author,pages){
+let readStatus = document.getElementById('read-status');
+let bookList = document.querySelector('.table-body');
+
+
+function Book(title,author,pages, haveRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveRead = false;
+    this.haveRead = haveRead;
 }
 
 function addBookToLibrary(book){
+    book = new Book();
+    book.title = document.getElementById('book-title').value;
+    book.author = document.getElementById('author').value;
+    book.pages = document.getElementById('num-pages').value;
+
+    if (readStatus.checked){
+        book.haveRead = "read"
+    }
+    else{
+        book.haveRead = "not yet read"
+    }
     myLibrary.push(book);
+}
+
+function displayLibrary(){
+    let j=1;
+    bookList.innerText = "";
+    myLibrary.forEach( (item,i,arr) => {
+        bookList.innerText += `${j++}. ${arr[i].title}, written by ${arr[i].author}, ${arr[i].pages}, ${arr[i].haveRead} \n\n`;        
+    });
 }
 
 addBook.addEventListener("click", (e) =>
 {
     e.preventDefault();
-    bookTitle = document.getElementById('book-title').value;
-    bookAuthor = document.getElementById('author').value;
-    numPages = document.getElementById('num-pages').value;
-    book = new Book(bookTitle,bookAuthor,numPages);
     addBookToLibrary(book);
-
-    console.log(book);
+    displayLibrary();
 });
 
+removeBook.addEventListener("click", (e) =>{
 
-//sample library
-book = new Book("The Pirates of the Carribean", "John Pogi", "300 pages");
-addBookToLibrary(book);
+    e.preventDefault()
+    myLibrary.pop();
+    displayLibrary();
+
+});
+
+displayLibrary();
